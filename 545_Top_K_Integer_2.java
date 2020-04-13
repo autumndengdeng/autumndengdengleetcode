983cf77dca1d843c90925da7fb3e00e89642c50a
Qiuyu Deng <qiuyudeng@Qiuyus-MacBook-Pro.local>
2020-04-13 10:50:43 -0700

    edited 545

--- 545_Top_K_Integer_2.java
+++ 545_Top_K_Integer_2.java
@@ -1,1 +1,26 @@
 //545 Top K Integer 2
+//实现一个数据结构，提供下面两个接口
+//1 add（number) 添加元素
+//2 topk() 返回前k大的数
+
+public class Solution {
+	int[] A;
+	int n = 0;
+	int size;
+
+	public Solution (int k) {
+		A = new int[k];
+		size = k;
+	}
+
+	private void add (int num) {
+		if (n < size) {
+			offer (num);
+		} else {
+			if (num > A[0]) {
+				poll();
+				offer(num);
+			}
+		}
+	}
+}
